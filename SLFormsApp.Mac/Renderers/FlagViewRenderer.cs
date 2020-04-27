d717f6fd00ce556588f4203cc8d832a4f463a764
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2020-04-27 20:11:35 +0300

    add flag renderer for mac

---
+++ SLFormsApp.Mac/Renderers/FlagViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿
+using SLFormsApp.Mac.Renderers;
+using SLFormsApp.Mac.Views;
+using SLFormsApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(FlagView), typeof(FlagViewRenderer))]
+namespace SLFormsApp.Mac.Renderers
+{
+    public class FlagViewRenderer : ViewRenderer<FlagView, NSFlagView>
+    {
+        NSFlagView uiCameraPreview;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<FlagView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                // Unsubscribe
+            }
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    uiCameraPreview = new NSFlagView();
+                    SetNativeControl(uiCameraPreview);
+                }
+                // Subscribe
+            }
+        }
+    }
+}
---
+++ SLFormsApp.Mac/Views/NSFlagView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using AppKit;
+using CoreGraphics;
+using SLFormsApp.Helpers;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace SLFormsApp.Mac.Views
+{
+    public class NSFlagView : NSView
+    {
+        public NSFlagView()
+        {
+        }
+
+        public override void Layout()
+        {
+            base.Layout();
+            Layer.BackgroundColor = NSColor.FromRgb(17, 52, 147).CGColor;
+        }
+
+        public override void DrawRect(CGRect dirtyRect)
+        {
+            base.DrawRect(dirtyRect);
+
+            var starSize = Bounds.Height / 10;
+            var radius = Bounds.Height / 3;
+            var count = 12;
+            var anglesCount = 5;
+
+            using (CGContext context = NSGraphicsContext.CurrentContext.CGContext)
+            {
+                NSColor.FromRgb(247, 205, 70).SetFill();
+
+                foreach (var star in FlagHelper.GetStars(count, (float)radius, anglesCount, (float)starSize, new Xamarin.Forms.Point(Bounds.Width / 2, Bounds.Height / 2)))
+                {
+                    var path = new CGPath();
+                    path.AddLines(star.Select(x => x.ToPointF()).ToArray());
+                    path.CloseSubpath();
+                    context.AddPath(path);
+                }
+                context.FillPath();
+            }
+        }
+    }
+}
